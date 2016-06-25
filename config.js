92b7a3801ab6b09c597d1fb4f68b0283f7298b1a
Emeka Onyenezido <sleonvictor@gmail.com>
2016-06-25 07:20:27 +0100

    implemented backend server using NodeJs, ExpressJs and MongoDB

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://localhost:27017/iso8583Database',
+	'secret': 'RjssfrkC9Nnx7u45DsqmhZxVDR3YZWGE6CHhvy4X9HkTCamvZqpmNL3pJt9UArMKckbfmkAXJQuPTnTNhNkVJuPEhPdaFR75y9P6j7tuTAtVLucnEW8DBDeK'
+};
