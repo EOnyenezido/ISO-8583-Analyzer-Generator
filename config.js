567906643dd9c5eb0101f6ba7899bf28edcca87f
Emeka Onyenezido <sleonvictor@gmail.com>
2016-06-29 19:30:41 +0100

    Changed the database from localhost to a cloud service on mongolab for testing

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://127.0.0.1:27017/iso8583Database',
+	'database': 'mongodb://sleonvictor:@Dx9GbUwy@ds023654.mlab.com:23654/iso8583database',
 	'secret': 'RjssfrkC9Nnx7u45DsqmhZxVDR3YZWGE6CHhvy4X9HkTCamvZqpmNL3pJt9UArMKckbfmkAXJQuPTnTNhNkVJuPEhPdaFR75y9P6j7tuTAtVLucnEW8DBDeK'
 };
